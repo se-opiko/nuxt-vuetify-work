17c8e773e46be087a5d52f601a6770897b71a886
se-opiko <opiopi206st@gmail.com>
2023-12-16 15:40:13 +0900

    indexページの作成

--- app.vue
+++ app.vue
@@ -1,13 +1,7 @@
 <template>
   <div>
     <v-app>
-      <v-card class="mx-auto" max-width="344" hover>
-        <v-card-item>
-          <v-card-title> Nuxt3 + Vuetify サンプル</v-card-title>
-          <v-card-subtitle> サンプル </v-card-subtitle>
-        </v-card-item>
-        <v-card-text> サンプル </v-card-text>
-      </v-card>
+      <nuxt-page />
     </v-app>
   </div>
 </template>
