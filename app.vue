adb2ccdcdb2be663ad97e1c0eba3ee44aea1596d
se-opiko <85811817+se-opiko@users.noreply.github.com>
2023-12-16 15:14:28 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-app>
+      <v-card class="mx-auto" max-width="344" hover>
+        <v-card-item>
+          <v-card-title> Nuxt3 + Vuetify サンプル</v-card-title>
+          <v-card-subtitle> サンプル </v-card-subtitle>
+        </v-card-item>
+        <v-card-text> サンプル </v-card-text>
+      </v-card>
+    </v-app>
+  </div>
+</template>
